a879efe9c6832fcbcf7c8f3c74c2d137f9cd8f52
mitian233 <mitian233@yahoo.co.jp>
2024-08-25 17:06:30 +0800

    chore: Update meta tags and dependencies for Twitter/X transparent avatar upload tool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: 'Twitter/X transparent avatar upload tool',
-        meta: [
-            { charset: 'utf-8' },
-            { key: 'description', name: 'description', content: 'Upload a profile image to Twitter that has a transparent background.' },
-        ],
+    app: {
+        head: {
+            title: 'Twitter/X transparent avatar upload tool',
+            viewport: 'width=device-width, initial-scale=1',
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'og:title', content: 'Twitter/X transparent avatar upload tool'},
+                {name: 'og:site_name', content: 'Twitter/X transparent avatar upload tool'},
+                {name: 'og:description', content: 'Upload a profile image to Twitter that has a transparent background.'},
+                {name: 'twitter:card', content: 'summary_large_image'},
+                {name: 'twitter:title', content: 'Twitter/X transparent avatar upload tool'},
+                {name: 'twitter:creator', content: '@9_bishi'},
+                {name: 'twitter:description', content: 'Upload a profile image to Twitter that has a transparent background.'},
+                {name: 'description', content: 'Upload a profile image to Twitter that has a transparent background.'},
+            ],
+        },
     },
-  },
-  compatibilityDate: '2024-04-03',
-  css: ['~/main.css'],
-  devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    compatibilityDate: '2024-04-03',
+    css: ['~/main.css'],
+    devtools: {enabled: true},
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
-  modules: [],
+    gtag: {
+        id: 'G-PTV191T31V'
+    },
+    modules: ['nuxt-gtag'],
 })
